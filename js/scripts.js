2749a4d8db08a2982cc56cdb6f183c4044d801a4
Bimmr - Randy <bimmr6696@gmail.com>
2020-01-20 15:52:12 -0500

    Code Cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -329,15 +329,14 @@ function isLightColor(color) {
     // If RGB --> Convert it to HEX: http://gist.github.com/983661
     color = +("0x" + color.slice(1).replace(
       color.length < 5 && /./g, '$&$&'
-    )
-             );
+    ));
 
     r = color >> 16;
     g = color >> 8 & 255;
     b = color & 255;
   }
 
-  // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+  // HSP equation from http://alienryderflex.com/hsp.html
   hsp = Math.sqrt(
     0.299 * (r * r) +
     0.587 * (g * g) +
@@ -345,14 +344,11 @@ function isLightColor(color) {
   );
 
   // Using the HSP value, determine whether the color is light or dark
-  if (hsp>127.5) {
-
+  if (hsp>127.5)
     return true;
-  }
-  else {
-
+  else
     return false;
-  }
+
 }
 
 //Copy text to clipboard
