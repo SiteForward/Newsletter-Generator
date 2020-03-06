8f11af73194d221075ce70e3289c466f9a54fa95
Bimmr - Randy <bimmr6696@gmail.com>
2020-03-06 10:28:53 -0500

    Added editHTML Toggle to toolbar

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ var quillSettingsText = {
       [{ 'header': [1, 2, 3, 4, false] }, {'align': []}],
       [{ 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}, 'bold', 'italic', 'underline'],
       ['link', 'image'],
+      ['code']
     ]
   },
   bounds: '#main',
@@ -18,7 +19,8 @@ var quillSettingsPost = {
     toolbar: [
       [{ 'header': [1, 2, 3, 4, false] }, {'align': []}],
       [{ 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}, 'bold', 'italic', 'underline'],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }, 'link']
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }, 'link'],
+      ['code']
     ]
   },
   bounds: '#main',
@@ -28,7 +30,8 @@ var quillSettingsHeader = {
   modules: {
     toolbar:[
       [ {'align': []}, { 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}],
-      ['bold', 'italic', 'underline']
+      ['bold', 'italic', 'underline'],
+      ['code']
     ]
   },
   bounds: '#main',
@@ -194,8 +197,24 @@ Vue.component('editabletext', {
       this.quill.getModule('toolbar').addHandler('color', (value) => {
         if (value == 'custom-color')
             value = prompt('Enter Hex/RGB/RGBA');
-
         this.quill.format('color', value);
+      });
+      this.quill.getModule('toolbar').addHandler('code', (value) => {
+        app.editHTML = true;
+        setTimeout(function(){
+
+          var toggleInputs = document.querySelectorAll('.editHTML');
+          toggleInputs.forEach( e => {
+            var div = document.createElement('div');
+            div.classList.add("editHTMLClose");
+            e.parentNode.appendChild(div);
+            div.addEventListener("click", function(){
+              app.editHTML = false;
+              var closeBtns = document.querySelectorAll('.editHTMLClose');
+              closeBtns.forEach(e => e.remove());
+            });
+          });
+        }, 1);
       });
     }
   }
