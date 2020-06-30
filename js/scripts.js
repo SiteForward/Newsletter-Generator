0385b202e3f8550bfad221733a8987ad52e32cf5
Randy Bimm <Bimmr6696@gmail.com>
2020-06-30 13:08:24 -0400

    - Cleaned up broken lineheight - Removed scrollbar on smaller views

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ Quill.register(SizeStyle, true);
 var quillSettingsText = {
   modules: {
     toolbar: [
-      [{ 'header': [1, 2, 3, 4, false] }, { 'size': ['.75em', false, '1.25em', '1.5em'] }, 'lineheight', {'align': []}],
+      [{ 'header': [1, 2, 3, 4, false] }, { 'size': ['.75em', false, '1.25em', '1.5em'] }, {'align': []}],
       [{ 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}, 'bold', 'italic', 'underline'],
       ['link', 'image', 'video'],
       ['clean', 'code']
@@ -23,7 +23,7 @@ var quillSettingsText = {
 var quillSettingsPost = {
   modules: {
     toolbar: [
-      [{ 'header': [1, 2, 3, 4, false] },  { 'size': ['.75em', false, '1.25em', '1.5em'] }, 'lineheight', {'align': []}],
+      [{ 'header': [1, 2, 3, 4, false] },  { 'size': ['.75em', false, '1.25em', '1.5em'] }, {'align': []}],
       [{ 'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}, 'bold', 'italic', 'underline'],
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
       ['link', 'image'],
