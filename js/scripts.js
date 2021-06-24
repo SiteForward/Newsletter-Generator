9b432b073a6b36d2cca1a45a1f22389e64e0c8cb
Randy Bimm <bimmr6696@gmail.com>
2021-06-24 13:12:49 -0400

    Edited Inline Editor

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,8 @@ let tinyMCE_settings = {
     default_link_target: '_blank',
     link_default_protocol: 'https',
     paste_as_text: true,
-    contextmenu: 'undo redo | help',
-    toolbar: 'formatselect fontsizeselect | bold italic underline | align lineheight | forecolor backcolor | numlist  bullist | superscript subscript | image | removeformat code',
+    contextmenu: false,
+    toolbar: ['formatselect fontsizeselect | bold italic underline | align lineheight', ' forecolor backcolor removeformat | numlist  bullist | superscript subscript | image | undo redo | code'],
     block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;',
     fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt 48pt',
     setup: function(editor) {
