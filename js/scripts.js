be4f4dbf6b1aacff6d3d76c5ba4252c554bdf2be
Bimmr <Bimmr6696@gmail.com>
2024-06-05 09:55:15 -0400

    Added todo comment

--- js/scripts.js
+++ js/scripts.js
@@ -429,6 +429,8 @@ let app = new Vue({
 
     var style = document.createElement("style");
     this.$refs.newsletter.prepend(style);
+
+    //TODO: Remove this after replacing TinyMCE
     alert("The newsletter generator is currently unavailable.\n\nWe're sorry for any inconvience this causes and are hoping to get it back up and running by the end of June 2024.")
     
     
